ba59cc0b4b63ecb3eddf2e4cd44e8e9125c13b8c
Nicole <nicole.phan136@gmail.com>
2024-07-12 20:59:29 +0100

    Initial Commit

---
+++ a2FinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  a2FinalProject
+//
+//  Created by Nicole Phan on 12/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, group a2!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ a2FinalProject/a2FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  a2FinalProjectApp.swift
+//  a2FinalProject
+//
+//  Created by Nicole Phan on 12/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct a2FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
