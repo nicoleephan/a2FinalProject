c3f246ffeb39077986e157ef4b429af55a707621
Rakshana Devalla <rhd0039@gmail.com>
2024-07-19 15:58:02 -0400

    Final MVP

--- a2FinalProject/AboutView.swift
+++ a2FinalProject/AboutView.swift
@@ -1,33 +1,318 @@
-//
-//  AboutView.swift
-//  a2FinalProject
-//
-//  Created by Nicole Phan on 18/07/2024.
-//
 
 import SwiftUI
 
+
+
 struct AboutView: View {
-    var body: some View {
-    ZStack {
-            Color(hex: "#EADDCA")
-        .edgesIgnoringSafeArea(.all)
-      VStack {
-        Spacer()
-          .frame(height: UIScreen.main.bounds.height / 14)
-        Text("Meet the Team!")
-              .font(.largeTitle)
-              .fontWeight(.bold)
-              .multilineTextAlignment(.center)
-              .padding(.bottom, 21)
-              .foregroundColor(Color(hex: "#5C4033"))
     
+    var body: some View {
+        
+        ZStack {
+            
+            Color(hex: "#EADDCA")
+            
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            
+            ScrollView {
+                
+                VStack {
+                    
+                    Spacer()
+                    
+                        .frame(height: UIScreen.main.bounds.height / 144)
+                    
+                    
+                    
+                    Text("Meet the Team!")
+                    
+                        .font(.largeTitle)
+                    
+                        .fontWeight(.bold)
+                    
+                        .multilineTextAlignment(.center)
+                    
+                        .padding()
+                    
+                        .foregroundColor(Color(hex: "#5C4033"))
+                    
+                    
+                    
+                    Image("team")
+                    
+                        .resizable()
+                    
+                        .aspectRatio(contentMode: .fit)
+                    
+                        .padding()
+                    
+                    
+                    
+                    //                    VStack(spacing: 20) {
+                    
+                    VStack {
+                        
+                        HStack {
+                            
+                            Image("purba")
+                            
+                                .resizable()
+                            
+                                .frame(width: 50, height: 50)
+                            
+                                .clipShape(Circle())
+                            
+                                .padding(.bottom, 15)
+                            
+                            
+                            
+                            Text("Purba")
+                            
+                                .font(.headline)
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                            
+                            
+                            
+                            
+                            Spacer()
+                            
+                            
+                            
+                            Text("Top left")
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                                .font(.caption)
+                            
+                        }
+                        
+                        .padding(.horizontal)
+                        
+                        
+                        
+                        Text("Hi, I’m Purba! I’m from New Jersey and am a rising senior. In my free time, I like hanging out with my friends and binging TV shows. I decided to participate in KWK to learn more about Xcode and Swift to potentially make my own app and wanted to be apart of a supportive community.")
+                        
+                            .padding(.horizontal)
+                        
+                            .padding(.bottom, 15)
+                        
+                            .foregroundColor(Color(hex: "#5C4033"))
+                        
+                            .font(.caption)
+                        
+                    }
+                    
+                    .padding()
+                    
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color(hex: "#5C4033"), lineWidth: 2))
+                    
+                    .padding(.horizontal)
+                    
+                    
+                    
+                    VStack {
+                        
+                        HStack {
+                            
+                            Image("nicole")
+                            
+                                .resizable()
+                            
+                                .frame(width: 50, height: 50)
+                            
+                                .clipShape(Circle())
+                            
+                                .padding(.bottom, 15)
+                            
+                            
+                            
+                            Text("Nicole")
+                            
+                                .font(.headline)
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                            
+                            
+                            Spacer()
+                            
+                            
+                            
+                            Text("Top right")
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                                .font(.caption)
+                            
+                        }
+                        
+                        .padding(.horizontal)
+                        
+                        
+                        
+                        Text("Hey, I’m Nicole! I’m from the UK and will be studying Economics and Data Science at university this September. I enjoy hiking, reading, and journaling in my spare time. KWK helps me to challenge myself with coding in a such a short space of time, and who doesn't love a great sisterhood?")
+                        
+                            .padding(.horizontal)
+                        
+                            .padding(.bottom, 15)
+                        
+                            .foregroundColor(Color(hex: "#5C4033"))
+                        
+                            .font(.caption)
+                        
+                    }
+                    
+                    .padding()
+                    
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color(hex: "#5C4033"), lineWidth: 2))
+                    
+                    .padding(.horizontal)
+                    
+                    
+                    
+                    VStack {
+                        
+                        HStack {
+                            
+                            Image("bianca")
+                            
+                                .resizable()
+                            
+                                .frame(width: 50, height: 50)
+                            
+                                .clipShape(Circle())
+                            
+                                .padding(.bottom, 15)
+                            
+                            
+                            
+                            
+                            
+                            Text("Bianca")
+                            
+                                .font(.headline)
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                            
+                            
+                            Spacer()
+                            
+                            
+                            
+                            Text("Bottom left")
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                                .font(.caption)
+                            
+                        }
+                        
+                        .padding(.horizontal)
+                        
+                        
+                        
+                        Text("Hi, I’m Bianca! I’m from Florida and I’m a rising senior. I love fashion, reading and Lana del Rey. This is my 3rd year at KWK! Not only do I love learning programming but I absolutely adore the community :)")
+                        
+                            .padding(.horizontal)
+                        
+                            .padding(.bottom, 15)
+                        
+                            .foregroundColor(Color(hex: "#5C4033"))
+                        
+                            .font(.caption)
+                        
+                    }
+                    
+                    .padding()
+                    
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color(hex: "#5C4033"), lineWidth: 2))
+                    
+                    .padding(.horizontal)
+                    
+                    
+                    
+                    VStack {
+                        
+                        HStack {
+                            
+                            Image("nicole")
+                            
+                                .resizable()
+                            
+                                .frame(width: 50, height: 50)
+                            
+                                .clipShape(Circle())
+                            
+                                .padding(.bottom, 15)
+                            
+                            
+                            
+                            Text("Rakshana")
+                            
+                                .font(.headline)
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                            
+                            
+                            Spacer()
+                            
+                            
+                            
+                            Text("Top right")
+                            
+                                .foregroundColor(Color(hex: "#5C4033"))
+                            
+                                .font(.caption)
+                            
+                        }
+                        
+                        .padding(.horizontal)
+                        
+                        
+                        
+                        Text("Hi! I'm Rakshana! I'm a rising senior from New Jersey. I enjoy music, advocating for human rights, and my dog! I've been programming for 4 years and this is my 2nd summer at KWK. I love coding solutions to problems that I am passionate about, such as public health!")
+                        
+                            .padding(.horizontal)
+                        
+                            .padding(.bottom, 15)
+                        
+                            .foregroundColor(Color(hex: "#5C4033"))
+                        
+                            .font(.caption)
+                        
+                    }
+                    
+                    .padding()
+                    
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color(hex: "#5C4033"), lineWidth: 2))
+                    
+                    .padding(.horizontal)
+                    
+                    
+                    
+                    
+                }
+                
+            }
         }
-        .padding(.top, 20)
-        Spacer()
-      }
+        
     }
-}
-#Preview {
-    AboutView()
+    
+    
+    
+    struct AboutView_Previews: PreviewProvider {
+        
+        static var previews: some View {
+            
+            AboutView()
+            
+        }
+        
+    }
+    
 }
--- a2FinalProject/ContentView.swift
+++ a2FinalProject/ContentView.swift
@@ -1,149 +1,256 @@
-//
-//  ContentView.swift
-//  a2FinalProject
-//
-//  Created by Nicole Phan on 12/07/2024.
-//
-
-//
-//  ContentView.swift
-//  finalProjectExperimentDouble
-//
-//  Created by Rakshana then I, Nicole Phan, made a copy on 17/07/24
-//
-
-
 import SwiftUI
-//
-//@main
-//struct MyApp: App {
-//    init() {
-//        UITabBar.appearance().tintColor = UIColor(red: 0.36, green: 0.25, blue: 0.20, alpha: 1.0) // Hex #5C4033
-//    }
-//    
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
-//}
 
 struct ContentView: View {
+
+    init() {
+
+           let appearance = UITabBarAppearance()
+
+           appearance.configureWithOpaqueBackground()
+
+           appearance.backgroundColor = UIColor(Color(hex: "#EADDCA"))
+
+
+
+           UITabBar.appearance().scrollEdgeAppearance = appearance
+
+           UITabBar.appearance().standardAppearance = appearance
+
+       }
+
+    
+
   var body: some View {
+
       TabView {
+
         NavigationView {
+
           HomeView()
+
         }
+
         .tabItem {
+
           Image(systemName: "house")
+
           Text("Home")
+
         }
+
         NavigationView {
+
           calc()
+
         }
+
         .tabItem {
+
           Image(systemName: "plus.slash.minus")
+
           Text("Calculator")
+
         }
+
         NavigationView {
+
           ProfileView()
+
         }
+
         .tabItem {
+
           Image(systemName: "person")
+
           Text("Your Profile")
+
         }
-          NavigationView {
-                         AboutView()
-                      }
-                      .tabItem {
-                          Image(systemName: "network")
-                          Text("Meet the Team")
-                      }
+
+        NavigationView {
+
+          AboutView()
+
+        }
+
+         .tabItem {
+
+          Image(systemName: "network")
+
+            Text("Meet the Team")
+
+        }
+
       }
+
     }
+
   }
+
   struct HomeView: View {
+
     var body: some View {
+
       ZStack {
+
               Color(hex: "#EADDCA")
+
           .edgesIgnoringSafeArea(.all)
+
         VStack {
+
           Spacer()
+
             .frame(height: UIScreen.main.bounds.height / 14)
+
           Text("Welcome to AllerGenie")
+
             .font(.largeTitle)
+
             .fontWeight(.bold)
+
             .multilineTextAlignment(.center)
+
             .padding(.bottom, 21)
+
             .foregroundColor(Color(hex: "#5C4033"))
+
             
+
             Image("AllerGenie")
+
                 .resizable()
+
                 .aspectRatio(contentMode: .fit)
+
                // .padding()
+
             
+
             
+
           Text("Food allergies are a serious problem. Allergic reactions can occasionally be fatal, but can more often be preventable!")
+
             .font(.body)
+
             .fontWeight(.bold)
+
             .multilineTextAlignment(.center)
+
             .padding(.horizontal, 20)
+
             .foregroundColor(Color(hex: "#5C4033"))
+
             .padding()
+
             
+
             // will change now to go to profile page 18/7/24
+
             
+
             Text("To proceed, press the button below to go to your profile to add your allergen information in. Then, go to the calculator to type in foods to check if you are at risk.")
+
                 .font(.body)
+
                 .fontWeight(.bold)
+
                 .multilineTextAlignment(.center)
+
                 .padding(.horizontal, 20)
+
                 .foregroundColor(Color(hex: "#5C4033"))
+
             
+
             
+
             
+
           NavigationLink(destination: ProfileView()) {
+
             Text("Enter")
+
               .font(.headline)
+
               .fontWeight(.bold)
+
               .padding()
+
               .frame(maxWidth: 215)
+
               .background(Color(hex: "#C4A484"))
+
               .foregroundColor(.white)
+
               .cornerRadius(10)
+
               .padding(.horizontal, 40)
+
           }
+
           .padding(.top, 20)
+
           Spacer()
+
         }
+
       }
+
       .navigationBarHidden(true)
+
     }
+
   }
+
  
+
  
+
   struct ContentView_Previews: PreviewProvider {
+
     static var previews: some View {
+
       ContentView()
+
     }
+
   }
+
   extension Color {
+
     init(hex: String) {
+
       let scanner = Scanner(string: hex)
+
       _ = scanner.scanString("#")
+
       var rgb: UInt64 = 0
+
       scanner.scanHexInt64(&rgb)
+
       let r = Double((rgb >> 16) & 0xFF) / 255.0
+
       let g = Double((rgb >> 8) & 0xFF) / 255.0
+
       let b = Double(rgb & 0xFF) / 255.0
+
       self.init(red: r, green: g, blue: b)
+
     }
+
   }
+
+
+
+
+
 
 
 
 
   #Preview {
+
     ContentView()
+
   }
 
